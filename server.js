37106af0a374499d49a1010e563f9a75d2cb0420
Ankit Kaushik <ankit@studysolo.com>
2016-07-17 00:46:11 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication : function(req,res,next){
+
+		console.log('private route hit!');
+		next();
+
+	},
+
+	logger:function(req,res,next){
+		console.log(req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+//app.use(middleware.requireAuthentication);
+
+app.get('/aboutus',middleware.requireAuthentication,function(req,res){
+
+	res.send('This is about us not just you!!');
+
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT,function(){
+	console.log('express server started at ' + PORT);
+});
